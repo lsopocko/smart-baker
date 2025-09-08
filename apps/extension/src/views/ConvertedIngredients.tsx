abb34cd8ff678292c1b80ae27f0df3f2925c67b8
lukasz.sopocko <lukasz.sopocko@gmail.com>
2025-09-08 14:10:09 +0200

    feat: ingredients display in popup

--- apps/extension/src/views/ConvertedIngredients.tsx
+++ apps/extension/src/views/ConvertedIngredients.tsx
@@ -4,9 +4,16 @@ import type { ConvertedIngredient } from '../types';
 export const ConvertedIngredients: FC<{ convertedIngredients?: ConvertedIngredient }> = ({ convertedIngredients }) => {
     return (
         <div className="flex flex-col gap-1 flex-1 self-stretch">
-            {convertedIngredients?.map((ingredient) => (
-                <div className="flex capitalize font-light text-sm flex-1 items-center">{ingredient.converted?.ingredient}</div>
-            ))}
+            {convertedIngredients
+                ?.filter((c) => c.original?.ingredient)
+                .map((ingredient) => (
+                    <div className="flex capitalize font-light text-sm flex-1 items-center justify-between">
+                        <span>{ingredient.converted?.ingredient}</span>
+                        <span className="btn btn-outline">
+                            {ingredient.converted?.value} {ingredient.converted?.unit}
+                        </span>
+                    </div>
+                ))}
         </div>
     );
 };
